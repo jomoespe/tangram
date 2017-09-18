cc9920478e941cafb468fc3ae598036873d91523
Jomoespe <jomoespe@gmail.com>
2017-09-18 11:07:38 +0200

    Add configuration
    Added configuration, but at this moment configuration is hardcoded. A tangram.yaml file have been
    created as example.

--- Makefile
+++ Makefile
@@ -6,5 +6,4 @@ clean:
 	@rm -rf build/
 
 build: 
-	@CGO_ENABLED=0 GOOS=linux go build -ldflags "-s -w -X 'main.version=$(cat VERSION)' -X 'main.build=$(git rev-parse --short HEAD)' -X 'main.buildDate=$(date --rfc-3339=seconds)'" -a -installsuffix cgo -o build/tangram .
-
+	@CGO_ENABLED=0 GOOS=linux go build -ldflags "-s -w -X 'main.version=$(cat VERSION)' -X 'main.build=$(git rev-parse --short HEAD)' -X 'main.buildDate=$(date --rfc-3339=seconds)'" -a -installsuffix cgo -o build/tangram
--- main.go
+++ main.go
@@ -2,24 +2,45 @@ package main
 
 import (
 	"log"
-    "net/http"
+	"net/http"
 )
+
+type Configuration struct {
+	Timeout int
+	Routes  Route // TODO do this as an array
+}
+type Route struct {
+	Service string
+	Path    string
+}
 
 var (
 	version   = "unset"
 	build     = "undefined"
 	buildDate = "unknown"
-    address   = ":8000"
+	address   = ":8000"
+	conf      Configuration
 )
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
+}
+
+func loadConfiguration() {
+	routes := Route{Service: "dachop-index", Path: "/dachop/"}
+	conf = Configuration{
+		Timeout: 5000,
+		Routes:  routes}
+
 }
 
 func main() {
 	log.SetPrefix("The Tangram Composer ")
-    log.Printf("version: %s, build: %s, build date: %s", version, build, buildDate)
-    http.HandleFunc("/health", healthCheck)
-    log.Printf("Listening on %s", address)
-    log.Fatal(http.ListenAndServe(address, nil))
+	log.Printf("version: %s, build: %s, build date: %s", version, build, buildDate)
+	loadConfiguration()
+	log.Printf("configuration loaded: %s", conf)
+
+	http.HandleFunc("/health", healthCheck)
+	log.Printf("Listening on %s", address)
+	log.Fatal(http.ListenAndServe(address, nil))
 }
