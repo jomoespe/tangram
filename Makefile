8939957c35d4986fa319405b45655603c4731ae2
Jomoespe <jomoespe@gmail.com>
2017-09-18 10:33:38 +0200

    Add HTTP server support and health check entry point

--- Makefile
+++ Makefile
@@ -6,5 +6,5 @@ clean:
 	@rm -rf build/
 
 build: 
-	@CGO_ENABLED=0 GOOS=linux go build -o build/tangram -a -installsuffix cgo
+	@CGO_ENABLED=0 GOOS=linux go build -ldflags "-s -w -X 'main.version=$(cat VERSION)' -X 'main.build=$(git rev-parse --short HEAD)' -X 'main.buildDate=$(date --rfc-3339=seconds)'" -a -installsuffix cgo -o build/tangram .
 
--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-    "log"
+	"log"
+    "net/http"
 )
 
+var (
+	version   = "unset"
+	build     = "undefined"
+	buildDate = "unknown"
+    address   = ":8000"
+)
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-    log.SetPrefix("The Tangram Composer ")
-    log.Println("hello, world")
+	log.SetPrefix("The Tangram Composer ")
+    log.Printf("version: %s, build: %s, build date: %s", version, build, buildDate)
+    http.HandleFunc("/health", healthCheck)
+    log.Printf("Listening on %s", address)
+    log.Fatal(http.ListenAndServe(address, nil))
 }
